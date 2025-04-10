e22e61c0aae01dff86b25645dcfed681c92ebc66
Carolina Sasowsky <mc.sasowsky@gmail.com>
2025-04-10 09:21:55 -0300

    feat: se implementó el paginador para la vista de personajes

--- js/main.js
+++ js/main.js
@@ -149,4 +149,30 @@ const printCharactersComic = (arr, containerText, container) => {
 
 	container.innerHTML = box;
 
-  };
+	//PAGINADOR EN FUNCIÓN DE LOS PERSONAJES
+	paginationContainer.innerHTML = `
+              <button id="first-page-btn" class="button button px-4 py-2 rounded bg-blue-600 text-white mr-3 disabled:opacity-50 disabled:cursor-not-allowed" ${
+								offset === 0 && "disabled"
+							} onclick="firstPage(${() => fetchCharacters(input, order)})">
+              <i class="fas fa-angle-double-left"></i>
+              </button>
+              <button id="previews-page-btn" class="button px-4 py-2 rounded bg-gray-200 text-gray-800 mr-2 disabled:opacity-50 disabled:cursor-not-allowed" ${
+								offset === 0 && "disabled"
+							} onclick="previewsPage(${() => fetchCharacters(input, order)})">
+              <i class="fas fa-angle-left"></i>
+              </button>
+              <div class="button px-4 py-2 rounded bg-gray-100 text-gray-900 mr-2">
+                  <span id="page-number">${pageNumber}</span>
+              </div>
+              <button id="next-page-btn" class="button px-4 py-2 rounded bg-gray-200 text-gray-800 disabled:opacity-50 disabled:cursor-not-allowed" ${
+								offset === total - (total % 20) && "disabled"
+							} onclick="nextPage(${() => fetchCharacters(input, order)})">
+              <i class="fas fa-angle-right"></i>
+              </button>
+              <button id="last-page-btn" class="button px-4 py-2 rounded bg-blue-600 text-white ml-3 disabled:opacity-50 disabled:cursor-not-allowed" ${
+								offset === total - (total % 20) && "disabled"
+							} onclick="lastPage(${() => fetchCharacters(input, order)})">
+              <i class="fas fa-angle-double-right"></i>
+              </button>
+      `;
+};
